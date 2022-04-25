dd45baa43ee3819d06b5a8de2d63fc3e0318380d
wwlouis00 <wwlouis00@gmail.com>
2022-04-25 18:13:51 +0800

    Update CT 0425

--- AmplificationPlot_0425.py
+++ AmplificationPlot_0425.py
@@ -20,6 +20,8 @@ colorTab_More4 = ['#e8a5eb', '#facc9e', '#e8e948', '#1bb763',
                        '#f32020', '#fd9b09', '#406386', '#24a1a1',
                        '#1515f8', '#959697', '#744a20', '#7b45a5']
 
+
+
 class Ui_MainWindow(QtWidgets.QWidget):
     def browsefile(self):
         if not os.path.isdir('./result'):
@@ -33,17 +35,178 @@ class Ui_MainWindow(QtWidgets.QWidget):
     def calculate(self):
         self.big_well = []
         self.big_data = []
-        if self.baseline_start_time.text() == "" or self.baseline_end_time.text() == "":
-        print("yes")
+        if self.baseline_start_time.text() == "" or self.baseline_end_time.text() == "" or self.Input_N.text() == "":
+            QtWidgets.QMessageBox.critical(self, u"Waring", u"Please input 'Start time' and 'End time' and 'Threshold: N * Std: '", buttons=QtWidgets.QMessageBox.Ok, defaultButton=QtWidgets.QMessageBox.Ok)
+        self.df_raw = pd.read_csv(self.fname[0])
+        self.df_raw.reset_index(inplace=True)
 
+        self.df_raw.rename(columns={'time':'well_1', 'A1':'well_2', 'A2':'well_3', 'A3':'well_4',
+                            'A4':'well_5', 'A5':'well_6', 'A6':'well_7', 'A7':'well_8',
+                            'A8':'well_9', 'B1':'well_10', 'B2':'well_11', 'B3':'well_12',
+                            'B4':'well_13', 'B5':'well_14', 'B6':'well_15', 'B7':'well_16'},inplace = True)
+        self.df_raw.drop(labels=["B8"], axis="columns")
+
+        print("*" * 150)
+        print(self.df_raw)
+        print("*" * 150)
+
+        self.df_normalization = self.df_raw.copy()
+        self.get_accumulation_time()
+        self.normalize()
+        threshold_value = self.get_ct_threshold()
+        # UI顯示 16個CT值
+        self.Ct_value = self.get_ct_value(threshold_value)
+
+        self.A1_data = []
+        self.A2_data = []
+        self.A3_data = []
+        self.A4_data = []
+        self.A5_data = []
+        self.A6_data = []
+        self.A7_data = []
+        self.A8_data = []
+        self.B1_data = []
+        self.B2_data = []
+        self.B3_data = []
+        self.B4_data = []
+        self.B5_data = []
+        self.B6_data = []
+        self.B7_data = []
+        self.B8_data = []
+        self.time_array = []
+
+        for i in range(1, 17, 1):
+            self.big_data.append(self.df_normalization["well"+str(i)].rolling(window=5).mean())
+        self.all_well = pd.DataFrame(self.big_data)
+        self.move_finish = self.all_well.T
+        for i in range(0, len(self.move_finish.index), 1):
+            self.A1_data.append(self.move_finish.loc[i,'well1'])
+            self.A2_data.append(self.move_finish.loc[i,'well2'])
+            self.A3_data.append(self.move_finish.loc[i,'well3'])
+            self.A4_data.append(self.move_finish.loc[i,'well4'])
+            self.A5_data.append(self.move_finish.loc[i,'well5'])
+            self.A6_data.append(self.move_finish.loc[i,'well6'])
+            self.A7_data.append(self.move_finish.loc[i,'well7'])
+            self.A8_data.append(self.move_finish.loc[i,'well8'])
+            self.B1_data.append(self.move_finish.loc[i,'well9'])
+            self.B2_data.append(self.move_finish.loc[i,'well10'])
+            self.B3_data.append(self.move_finish.loc[i,'well11'])
+            self.B4_data.append(self.move_finish.loc[i,'well12'])
+            self.B5_data.append(self.move_finish.loc[i,'well13'])
+            self.B6_data.append(self.move_finish.loc[i,'well14'])
+            self.B7_data.append(self.move_finish.loc[i,'well15'])
+            self.B8_data.append(self.move_finish.loc[i,'well16'])
+
+        for j in range(0, len(self.move_finish.index), 1):
+            self.time_array.append(j / 2)
+    
+    def get_accumulation_time(self):
+        df_time = self.df_normalization['time']
+        time_ori = datetime.strptime(df_time[0], "%H:%M:%S")
+        time_delta = []
+        for time in df_time:
+            time_now = datetime.strptime(time, "%H:%M:%S")
+            time_delta.append((time_now - time_ori).seconds / 60)
+        self.df_normalization.insert(1, column="accumulation", value=time_delta)
+
+    def get_StdDev_and_Avg(self):
+        StdDev = []
+        Avg = []
+        for i in range(0, 16):
+            df_current_well = self.df_normalization[f'well_{i + 1}']
+            StdDev.append(df_current_well[int(self.baseline_start_time.text()) * 2:int(self.baseline_end_time.text()) * 2].std())
+            Avg.append(df_current_well[int(self.baseline_start_time.text()) * 2:int(self.baseline_end_time.text()) * 2].mean())
+        return StdDev, Avg
+
+    def normalize(self):
+        for i in range(0, 16):
+            df_current_well = self.df_raw[f'well_{i + 1}']
+            self.baseline = df_current_well[int(self.baseline_start_time.text()) * 2:int(self.baseline_end_time.text()) * 2].mean()
+            self.df_normalization[f'well{i + 1}'] = (self.df_raw[f'well_{i + 1}'] - self.baseline) / self.baseline
+            if(i<8):
+                print(f'A{i+1}'+" baseline value: " + str(self.baseline))
+            else:
+                print(f'B{i-7}'+" baseline value: " + str(self.baseline))
+
+            # print(f'well_{i+1}'+" 的baseline值: " + str(self.baseline))
+            # print(self.baseline)# normalized = (IF(t)-IF(b))/IF(b)
+        print("*"*100)
+
+    def get_ct_threshold(self):
+        threshold_value = []
+        StdDev, Avg = self.get_StdDev_and_Avg()
+        for i in range(0, 16):
+            threshold_value.append(int(self.Input_N.text()) * StdDev[i] + Avg[i])
+        return threshold_value
+
+    def get_ct_value(self, threshold_value):
+        Ct_value = []
+        for i in range(0, 16):
+            df_current_well = self.df_normalization[f'well_{i + 1}']
+            df_accumulation = self.df_normalization['accumulation']
+            try:
+                for j, row in enumerate(df_current_well):
+                    if row >= threshold_value[i]:
+                        thres_lower = df_current_well[j - 1]
+                        thres_upper = df_current_well[j]
+                        acc_time_lower = df_accumulation[j - 1]
+                        acc_time_upper = df_accumulation[j + 1]
+                        # linear regression
+                        x2 = acc_time_upper
+                        y2 = thres_upper
+                        x1 = acc_time_lower
+                        y1 = thres_lower
+                        y = threshold_value[i]
+                        x = (x2 - x1) * (y - y1) / (y2 - y1) + x1
+                        Ct_value.append(round(x, 2))
+                        # print(f"Ct of well_{i + 1} is {round(x, 2)}")
+                        break
+                    # if there is no Ct_value availible
+                    elif j == len(df_current_well) - 1:
+                        Ct_value.append("N/A")
+                        # print("Ct value is not available")
+            except Exception as e:
+                Ct_value.append("N/A")
+        return Ct_value
+    #重置計算
+    def reset_file(self):
+        if self.Input_file.text() == "" or self.Start_time.text() == "" or self.End_time.text() == "" or self.Input_N.text() == "":
+            QtWidgets.QMessageBox.critical(self, u"未輸入開始時間以及結束時間!", u"未開啟任何Csv檔案", buttons=QtWidgets.QMessageBox.Ok,defaultButton=QtWidgets.QMessageBox.Ok)
+        else:
+            self.calculate()
+    #存檔
+    def save_file(self):
+        #儲存失敗
+        if self.file_csv_input.text() == "":
+            QtWidgets.QMessageBox.critical(self, u"存取失敗", u"未開啟csv檔案", buttons=QtWidgets.QMessageBox.Ok,
+                defaultButton=QtWidgets.QMessageBox.Ok)
+        #儲存成功
+        else:
+            QtWidgets.QMessageBox.information(self, u"存取成功", u"已成功另存Excel檔案", buttons=QtWidgets.QMessageBox.Ok,
+                defaultButton=QtWidgets.QMessageBox.Ok)
+            print("Save data successful !!!")
+            #設置資料欄位
+            self.save_excel = pd.DataFrame({"A1": [self.Ct_value[0]], "A2": [self.Ct_value[1]],
+                                            "A3": [self.Ct_value[2]], "A4": [self.Ct_value[3]],
+                                            "A5": [self.Ct_value[4]], "A6": [self.Ct_value[5]],
+                                            "A7": [self.Ct_value[6]], "A8": [self.Ct_value[7]],
+                                            "B1": [self.Ct_value[8]], "B2": [self.Ct_value[9]],
+                                            "B3": [self.Ct_value[10]], "B4": [self.Ct_value[11]],
+                                            "B5": [self.Ct_value[12]], "B6": [self.Ct_value[13]],
+                                            "B7": [self.Ct_value[14]], "B8": [self.Ct_value[15]]}
+                , index=["CT_Value"])
+            #儲存資料以及存取位置
+            self.move_finish.to_csv('./result/Display_result/CT_Value_'+ now_output_time + '_MA_data.csv', encoding="utf_8_sig")
+            self.save_excel.T.to_csv('./result/Display_result/CT_Value' + now_output_time + "all_well.csv", encoding="utf_8_sig")
+    #清除顯示
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(950, 783)
+        MainWindow.resize(959, 798)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.Threshold = QtWidgets.QGroupBox(self.centralwidget)
-        self.Threshold.setGeometry(QtCore.QRect(10, 354, 931, 403))
+        self.Threshold.setGeometry(QtCore.QRect(10, 354, 931, 409))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(False)
@@ -60,7 +223,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.Threshold_function.setFont(font)
         self.Threshold_function.setObjectName("Threshold_function")
         self.Baseline = QtWidgets.QGroupBox(self.Threshold_function)
-        self.Baseline.setGeometry(QtCore.QRect(10, 174, 321, 67))
+        self.Baseline.setGeometry(QtCore.QRect(10, 42, 321, 67))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(False)
@@ -113,6 +276,28 @@ class Ui_MainWindow(QtWidgets.QWidget):
         font.setWeight(50)
         self.update_calculation.setFont(font)
         self.update_calculation.setObjectName("update_calculation")
+        self.widget = QtWidgets.QWidget(self.Threshold_function)
+        self.widget.setGeometry(QtCore.QRect(20, 174, 301, 26))
+        self.widget.setObjectName("widget")
+        self.Threshold_N = QtWidgets.QGridLayout(self.widget)
+        self.Threshold_N.setContentsMargins(0, 0, 0, 0)
+        self.Threshold_N.setObjectName("Threshold_N")
+        self.label_threshold = QtWidgets.QLabel(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_threshold.setFont(font)
+        self.label_threshold.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_threshold.setObjectName("label_threshold")
+        self.Threshold_N.addWidget(self.label_threshold, 0, 0, 1, 1)
+        self.Input_N = QtWidgets.QLineEdit(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.Input_N.setFont(font)
+        self.Input_N.setAlignment(QtCore.Qt.AlignCenter)
+        self.Input_N.setObjectName("Input_N")
+        self.Threshold_N.addWidget(self.Input_N, 0, 1, 1, 1)
         self.verticalLayoutWidget = QtWidgets.QWidget(self.Threshold)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(380, 108, 311, 289))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
@@ -386,6 +571,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.Plot_display.setObjectName("Plot_display")
         self.gridLayout_2.addWidget(self.Plot_display, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -396,12 +582,13 @@ class Ui_MainWindow(QtWidgets.QWidget):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Amplification Plot"))
-        self.Threshold.setTitle(_translate("MainWindow", "Threshold"))
+        self.Threshold.setTitle(_translate("MainWindow", "Threshold Cycle Calculation"))
         self.Threshold_function.setTitle(_translate("MainWindow", "Threshold"))
         self.Baseline.setTitle(_translate("MainWindow", "Baseline(Min)"))
         self.baseline_start.setText(_translate("MainWindow", "Start:"))
         self.baseline_end.setText(_translate("MainWindow", "End:"))
         self.update_calculation.setText(_translate("MainWindow", "Update Calculations"))
+        self.label_threshold.setText(_translate("MainWindow", "Threshold: N  * Std: "))
         item = self.result_value.verticalHeaderItem(0)
         item.setText(_translate("MainWindow", "A1"))
         item = self.result_value.verticalHeaderItem(1)
@@ -466,10 +653,10 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.CT_B8.setText(_translate("MainWindow", "B8"))
         self.version.setText(_translate("MainWindow", "WinnoZ. V. 1.0.2"))
         self.Plot.setTitle(_translate("MainWindow", "Plot"))
+
 if __name__ == '__main__':
     print("*" * 50)
     print("Now Time: " + now_output_time)
-    print("*" * 50)
     app = QApplication(sys.argv)
     mainWindows = QMainWindow()
     ui = Ui_MainWindow()
--- AmplificationPlot_Web.py
+++ AmplificationPlot_Web.py
@@ -1,2 +1,29 @@
-import plotly.graph_objs as go
 import pandas as pd
+
+df = pd.DataFrame({
+  "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+  "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
+  "Number Eaten": [2, 1, 3, 1, 3, 2],
+})
+
+
+# Plotly Express
+
+import plotly.express as px
+
+fig = px.bar(df, x="Fruit", y="Number Eaten", color="Contestant", barmode="group")
+fig.show()
+
+
+# Graph Objects
+
+import plotly.graph_objects as go
+
+fig = go.Figure()
+for contestant, group in df.groupby("Contestant"):
+    fig.add_trace(go.Bar(x=group["Fruit"], y=group["Number Eaten"], name=contestant,
+      hovertemplate="Contestant=%s<br>Fruit=%%{x}<br>Number Eaten=%%{y}<extra></extra>"% contestant))
+fig.update_layout(legend_title_text = "Contestant")
+fig.update_xaxes(title_text="Fruit")
+fig.update_yaxes(title_text="Number Eaten")
+fig.show()
